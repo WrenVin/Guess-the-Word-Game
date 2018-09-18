71aeea3b22b5cff2d9a9057d8cd8c1862a4af38c
WrenVin <31417492+WrenVin@users.noreply.github.com>
2018-09-18 07:56:19 -0500

    Updated main file

--- main.py
+++ main.py
@@ -1,87 +1,53 @@
 #Hangman
-import random
-import string
-def main():
-    hChars = ['     ', '0', '|', '/', '\\', '/', ' \\']
-    words = ("beach", 'jerky', 'arise', 'space', 'cloud', 'house', 'trial', 'earth', 'money')
-    finalWord = random.choice(words)
-    hangman = '''{}{}
-    {}{}{}
-    {}{}'''.format(hChars[0], hChars[1], hChars[3], hChars[2], hChars[4], hChars[5], hChars[6])
-    
-    for i in range(len(finalWord)):
-        pass
-    
-
-    a = string.ascii_letters
-    a1 = finalWord[0]
-    a2 = finalWord[1]
-    a3 = finalWord[2]
-    a4 = finalWord[3]
-    a5 = finalWord[4]
-    
-    
-
-
-    word = [a1]
-    
-    
-
   
-    endWord = ""
-    print("Welcome to Hangman Digital! Make sure to only enter lowercase letters. \nYou only have 20 tries. Make sure to only enter ONE letter at a time. \nHINT: The word is 5 letters long.")
-    letterWord = ""
-    for i in range(20):
+from random import choice
+from string import ascii_letters
+
+class Hangman:
+    def __init__(self):
+        self.words = {"beach":'Somewhere near an ocean', 'jerky':'A dried piece of meat', 'space': 'The last frontier', 'clouds':'in the sky', 'house':'A building you spend alot of time in', 'trial':'Something you go though for commiting a crime', 'Earth':'Big, Blue, and green', 'money':'Somthing everyone wants more of', 'train':'the invention that laid the "Tracks" for the future', 
+        'about':'close to or almost', 'knowledge':'something the really smart people have', 'people':'You, your aunt, your baby brother, you mean ELA teacher, etc.', 'steal':'somthing that criminals do', 'goodness':'somthing that we need more of', 'child':'young human', 'hands':'you have two on your arms', 'place':'word for somewhere you are', 'weeks':'there are 56 of these in a year', 'government':'somthing that controls our country', 
+        'company':'another word for a large buisness', 'math':'somthing we learn at school'}
+        for self.word in self.words.keys():
+            self.final_word = self.word
+            #print(self.final_word)
+            break
+        self.b = self.words.get('{}'.format(self.final_word))
+        self.char_num = 0
+        self.word = self.final_word[self.char_num]
         
-        if endWord == finalWord:
-            break
-        if len(endWord) > 4:
-            break
-        letterInput = input(":{}".format(letterWord))
-        for letter in word:
-            if letterInput == letter:
-                letterWord += letter
-                if letterInput == a1:
-                    endWord += letterInput
-                    word.remove(a1)
-                    word.append(a2)
-                if letterInput == a2:
-                    word.remove(a2)
-                    word.append(a3)
-                    endWord += letterInput
-                if letterInput == a3:
-                    word.remove(a3)
-                    word.append(a4)
-                    endWord += letterInput
-                if letterInput == a4:
-                    word.remove(a4)
-                    word.append(a5)
-                    endWord += letterInput
-                if letterInput == a5:
-                    word.remove(a5)
-                    endWord += letterInput
-    if endWord == finalWord:
-        print("You have Won! Final word: {}".format(finalWord))
-         
-    elif endWord != finalWord:
-        print("You Lost...")
-    return endWord
-                    
-end = main()
-def cls():
-    print('\n' * 200)
-
-def trueEnd(l):
-    while True:        
-        k = input("Play again?: yes/no:")  
-        if k == "yes":
-            cls()
-            end = main()
+    def main(self):
+        self.lost = True
+        self.amount_found_word = ''
+        print("Welcome to Hangman Digital! Make sure to only enter lowercase letters. \nYou only have 30 tries. Make sure to only enter ONE letter at a time.")
+        hint = input('Would you like a hint?(y/n):')
+        if hint == 'y':
+            print(self.b)
+        for i in range(30):
+            self.input_ = input(">>>{}".format(self.amount_found_word))
+            try:
+                if self.input_ == self.word:
+                    self.char_num += 1
+                    self.word = self.final_word[self.char_num]
+                    self.amount_found_word += self.input_
+                    self.input_ += self.word
+                if self.input_ == self.final_word:
+                    raise IndexError
+            except IndexError:
+                self.lost = False
+                break
+        if self.lost:
+            print('You Lost...')
         else:
-            break
-    
-trueEnd(end)
-
+            print('You Won!!! Final Word:{}'.format(self.final_word))
+ 
+while True:
+    hangman = Hangman()
+    hangman.main()
+    restart = input('Again?(y/n):')
+    if restart != 'y':
+        break
+  
 
 
 
